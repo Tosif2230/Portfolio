31159b307a2db810b618377c3a84e3d606854a9d
Tosif2230 <techie.tosif@gmail.com>
2026-01-31 16:36:26 +0530

    Navbar create with responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
-import React from "react";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Services from "./components/Services";
-import Contact from "./components/Contect";
-import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar.jsx";
+import AOS from 'aos'
+import 'aos/dist/aos.css';
 
-function App() {
+
+const App = () => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(()=>{
+    AOS.init({
+      duration: 1000,
+      once: false,
+      offset: 100
+    });
+    document.documentElement.classList.add('dark');
+  },[]);
+
+  const toggleDarkMode = () => {
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    document.documentElement.classList.toggle('dark');
+  }
+
   return (
-    <div>
-      <Header />
-      <Home />
-      <About />
-      <Skills />
-      <Services />
-      <Contact />
-      <Footer />
+    <div className={darkMode ? 'bg-linear-to-br from-gray-900 via-[#0d182e] to-gray-900 min-h-screen' :
+      'bg-linear-to-br from-gray-50 to-blue-50 min-h-screen'
+    }>
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="py-6 text-center bg-white border-t">
-      <p className="text-gray-600">© Mohammed Tosif 2026</p>
-    </footer>
-  );
-}
-
-export default Footer;
