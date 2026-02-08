2df4765986d22b53a14edac29172596c8cf17f4c
Tosif2230 <techie.tosif@gmail.com>
2026-02-08 15:44:19 +0530

    Footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,51 @@
 import { useEffect, useState } from "react";
-import AOS from 'aos'
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
 import About from "./components/About.jsx";
 import Skills from "./components/Skills.jsx";
 import Project from "./components/Project.jsx";
 import Contact from "./components/Contact.jsx";
-
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     AOS.init({
       duration: 1000,
       once: false,
-      offset: 100
+      offset: 100,
     });
-    document.documentElement.classList.add('dark');
-  },[]);
+    document.documentElement.classList.add("dark");
+  }, []);
 
-  useEffect(()=>{
-    AOS.refresh()
-  },[darkMode])
+  useEffect(() => {
+    AOS.refresh();
+  }, [darkMode]);
 
   const toggleDarkMode = () => {
     const newMode = !darkMode;
     setDarkMode(newMode);
-    document.documentElement.classList.toggle('dark');
-  }
+    document.documentElement.classList.toggle("dark");
+  };
 
   return (
-    <div className={darkMode ? 'bg-linear-to-br from-gray-900 via-[#0d182e] to-gray-900 min-h-screen' :
-      'bg-linear-to-br from-gray-50 to-blue-50 min-h-screen'
-    }>
-      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
-      <Hero darkMode={darkMode}/>
-      <About darkMode={darkMode}/>
-      <Skills darkMode={darkMode}/>
-      <Project darkMode={darkMode}/>
+    <div
+      className={
+        darkMode
+          ? "bg-linear-to-br from-gray-900 via-[#0d182e] to-gray-900 min-h-screen"
+          : "bg-linear-to-br from-gray-50 to-blue-50 min-h-screen"
+      }
+    >
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Hero darkMode={darkMode} />
+      <About darkMode={darkMode} />
+      <Skills darkMode={darkMode} />
+      <Project darkMode={darkMode} />
       <Contact darkMode={darkMode} />
+      <Footer darkMode={darkMode} />
     </div>
   );
 };
